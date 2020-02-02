7e5ec374f440e5d96f9e743ad9ecd36c90e3d850
Sujan Maharjan <plumin.canxa@gmail.com>
2020-02-02 22:57:38 +0545

    understanding WebService - alternative for RestTemplate

--- movie-catalog-service/src/main/java/sujan/smiles/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/sujan/smiles/moviecatalogservice/resources/MovieCatalogResource.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import sujan.smiles.moviecatalogservice.model.CatalogItem;
 import sujan.smiles.moviecatalogservice.model.Movie;
 import sujan.smiles.moviecatalogservice.model.Rating;
@@ -18,8 +19,17 @@ import java.util.stream.Collectors;
 @RequestMapping("catalog")
 public class MovieCatalogResource {
 
+    private final RestTemplate restTemplate;
+
+    /*
+     * Can use WebClient instead of RestTemplate
+     */
+//    private final WebClient.Builder webClientBuilder;
+
     @Autowired
-    private RestTemplate restTemplate;
+    public MovieCatalogResource(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     @GetMapping("{userId}")
     public List<CatalogItem> getCatalogs(@PathVariable("userId") String userId) {
@@ -35,7 +45,6 @@ public class MovieCatalogResource {
         //for each movie Id, call movie Info service and get details
         //put them all together
 
-
         return ratings.stream()
                 .map(rating -> {
                     Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
@@ -45,6 +54,24 @@ public class MovieCatalogResource {
                             rating.getRating());
                 })
                 .collect(Collectors.toList());
+
+//        return ratings.stream()
+//                .map(rating -> {
+//
+//                    Movie movie = webClientBuilder.build()
+//                            .get()
+//                            .uri("http://localhost:8082/movies/" + rating.getMovieId())
+//                            .retrieve()
+//                            .bodyToMono(Movie.class)
+//                            .block();
+//
+//                    return new CatalogItem(
+//                            movie.getName(),
+//                            "desc of" + rating.getMovieId(),
+//                            rating.getRating());
+//
+//                })
+//                .collect(Collectors.toList());
 
     }
 
--- rating-data-service/src/main/java/sujan/smiles/ratingdataservice/resources/RatingsResource.java
+++ rating-data-service/src/main/java/sujan/smiles/ratingdataservice/resources/RatingsResource.java
@@ -5,13 +5,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sujan.smiles.ratingdataservice.model.Rating;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("ratings")
 public class RatingsResource {
 
     @RequestMapping("{movieId}")
-    public Rating getRating(@PathVariable("movieId") String movieId) {
+    public Rating getRatingByMovieId(@PathVariable("movieId") String movieId) {
         return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("users/{userId}")
+    public List<Rating> getRatingByUserId(@PathVariable("userId") String userId) {
+
+        List<Rating> ratingList = Arrays.asList(
+                new Rating("m1", 4),
+                new Rating("m2", 3),
+                new Rating("m3", 4)
+        );
+
+        return ratingList;
     }
 
 }
